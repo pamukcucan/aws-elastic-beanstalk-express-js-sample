3ab26ec0125eb3f5f2d31443881ab2eea85450ba
pamukcucan <73982685+pamukcucan@users.noreply.github.com>
2021-11-24 12:32:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Uykucu Efsa artık uyansın!!:)'));
+app.get('/', (req, res) => res.send('Merhaba!!:)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
